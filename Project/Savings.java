c811d4bde7543262c97cfbd37b57d9410fd27862
Irina Smirnova <kmiranda.2000@gmail.com>
2021-06-24 19:58:55 -0400

    initial

---
+++ Project/Savings.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ * A class that represents a Savings account
+ * Must extend abstract Account class to inherit generic account properties and behaviors
+ *
+ */
+
+public class Savings extends Account{
+
+      // Akmal was here
+}
---
+++ Project/Utility.java
@@ -0,0 +1,25 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.io.*;
+import java.lang.ProcessBuilder.Redirect;
+import java.nio.file.FileSystemNotFoundException;
+import java.util.*;
+
+public class Utility {
+
+	Object o;
+	int a;
+
+	int b;
+	
+	
+	
+	public static List<String[]> readFromAFile(String customers) throws IOException {
+		
+
+
+	}}
+	
+	
+
+
