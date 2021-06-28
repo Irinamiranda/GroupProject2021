9b116acfc18a863a3b542fecc98a39084b77291d
“Bahar <bahar.a.mahmud@gmail.com>
2021-06-28 10:27:05 -0400

    Saving account changes

--- Project/Savings.java
+++ Project/Savings.java
@@ -6,6 +6,9 @@
  */
 
 public class Savings extends Account{
+    public static void main(String[] args) {
+        System.out.println("Your saving account is accumulating interest");
+    }
 
       // Akmal was here
 }
